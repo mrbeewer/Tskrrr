57a26ec411328b985ea4d3e9211ec3ff24146480
David Beeler <david.beeler@gmail.com>
2015-12-02 11:23:38 -0600

    App has been fixed

--- db/migrations.sql
+++ db/migrations.sql
@@ -2,3 +2,7 @@
 # connect
 # create a tasks table
 # tasks contain name (varchar(255)) and description (varchar(255))
+
+CREATE DATABASE tskrrr;
+\c tskrrr
+CREATE TABLE tasks (id SERIAL PRIMARY KEY, name VARCHAR(255), description VARCHAR(255));
--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,5 @@
 # insert a few rows into your table
+INSERT INTO tasks (name, description) VALUES ('David', 'Eat pizza');
+INSERT INTO tasks (name, description) VALUES ('Jan', 'Obsess over Davids Atom Settings');
+INSERT INTO tasks (name, description) VALUES ('Loki', 'sleep');
+INSERT INTO tasks (name, description) VALUES ('Thor', 'Eat');
